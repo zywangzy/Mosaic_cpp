fda7cb54373b14abef9fbad30feda5be9f9606bb
wangzyusc <wangzhiy@usc.edu>
2017-06-26 23:31:36 -0700

    Added some notation of new classes

--- mosaicGenerator.h
+++ mosaicGenerator.h
@@ -15,10 +15,28 @@ private:
     unordered_map<string, colorHistVector> img_lib;
     Mat src_img;
 public:
+    /**
+     * Constructor of mosaicGenerator.
+     * @param segment_obj the object of image segmentation result.
+     * @param basic_path the path of image library.
+     */
     mosaicGenerator(imgSegmentation& segment_obj, string basic_path);
+    /**
+     * Generate the final mosaic result. Iterate all chunks in segmentation result,
+     * get the optimal replacement image from library and return the result image.
+     * @return
+     */
     Mat generate();
 private:
+    /**
+     * Helper method to get all the colorHistVector objects from library.
+     */
     void library_reader();
+    /**
+     * Helper method to find the best matching hist in library for input hist.
+     * @param histVector given color histogram vector.
+     * @return the key of best matching hist in map.
+     */
     string find_target_in_lib(colorHistVector& histVector);
 };
 
